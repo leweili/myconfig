fee40c67d0d35ae6869d4d4f415a2a4ad38e4071
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2024-04-25 20:09:31 +0800

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -1,19 +1,22 @@
 #!/data/data/com.termux/files/usr/bin/sh
 pkg update
 #termux-change-repo
-pkg install wget vim termux-services -y
-wget https://mirror.ghproxy.com/https://github.com/ykxVK8yL5L/alist/releases/latest/download/alist-linux-musl-arm64.tar.gz
-tar -zxvf alist-linux-musl-arm64.tar.gz
-./alist admin set admin
+pkg instal termux-services tsu -y
+apt install -y wget dpkg
+#wget https://mirror.ghproxy.com/https://github.com/ykxVK8yL5L/alist/releases/latest/download/alist-linux-musl-arm64.tar.gz
+#tar -zxvf alist-linux-musl-arm64.tar.gz
+wget https://mirror.ghproxy.com/https://github.com/ykxVK8yL5L/alist/releases/latest/download/alist_0.0.1_termux_aarch64.deb -O alist.deb
+dpkg -i alist.deb
+alist admin set admin
 sed -i 's/5244/10002/g' data/config.json
-#./alist server
+#alist server
 #vi /etc/resolv.conf   nameserver 114.114.114.114
 mkdir -p $PREFIX/var/service/alist/log 
 ln -sf $PREFIX/share/termux-services/svlogger $PREFIX/var/service/alist/log/run
 #vi $PREFIX/var/service/alist/run
 echo '#!/data/data/com.termux/files/usr/bin/sh' > $PREFIX/var/service/alist/run
 echo 'exec 2>&1' >> $PREFIX/var/service/alist/run
-echo 'cd ~ && ~/alist server' >> $PREFIX/var/service/alist/run
+echo 'cd ~ && alist server' >> $PREFIX/var/service/alist/run
 chmod a+x $PREFIX/var/service/alist/run
 sv-enable alist 
 sv up alist 
