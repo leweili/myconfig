b58a5628acbb87b15c33993ad60674b6b70ac3ac
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2024-04-26 07:54:27 +0800

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -1,11 +1,16 @@
 #!/data/data/com.termux/files/usr/bin/sh
 pkg update
 #termux-change-repo
-pkg instal wget termux-services -y
+echo '安装依赖'
+pkg instal termux-services tsu -y
+apt install -y wget dpkg
+echo '安装Alist'
 wget https://mirror.ghproxy.com/https://github.com/ykxVK8yL5L/alist/releases/latest/download/alist_termux_aarch64.deb -O alist.deb
-pkg install alist.deb
+dpkg -i alist.deb
+echo '配置Alist，默认密码:admin,端口:10003'
 alist admin set admin
 sed -i 's/5244/10002/g' data/config.json
+echo '创建开机启动服务'
 mkdir -p $PREFIX/var/service/alist/log 
 ln -sf $PREFIX/share/termux-services/svlogger $PREFIX/var/service/alist/log/run
 echo '#!/data/data/com.termux/files/usr/bin/sh' > $PREFIX/var/service/alist/run
