0b78a0d123b018e8ad86aad1a935c88d82a41724
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2024-05-26 11:30:23 +0800

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -8,7 +8,7 @@ pip install requests
 echo '下载Python文件'
 wget https://mirror.ghproxy.com/https://github.com/ykxVK8yL5L/myconfig/raw/main/address.py -O address.py
 echo '安装Alist'
-wget https://mirror.ghproxy.com/https://github.com/ykxVK8yL5L/alist/releases/latest/download/alist_termux_aarch64.deb -O alist.deb
+wget https://mirror.ghproxy.com/https://github.com/ykxVK8yL5L/termux-packages/releases/latest/download/alist_1_aarch64.deb -O alist.deb
 dpkg -i alist.deb
 echo '配置Alist，默认密码:admin,端口:10002'
 alist admin set admin
