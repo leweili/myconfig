929def8882ca6dbfd55f6fa20bf49c5c9752c1d1
leweili <leweili@163.com>
2024-07-07 22:11:32 +0800

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -12,7 +12,7 @@ wget https://mirror.ghproxy.com/https://github.com/ykxVK8yL5L/termux-packages/re
 dpkg -i alist.deb
 echo '配置Alist，默认密码:admin,端口:10002'
 alist admin set admin
-sed -i 's/5244/10002/g' data/config.json
+//sed -i 's/5244/10002/g' data/config.json
 echo '创建开机启动服务'
 mkdir -p $PREFIX/var/service/alist/log 
 ln -sf $PREFIX/share/termux-services/svlogger $PREFIX/var/service/alist/log/run
