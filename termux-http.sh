824abb7f7fde2c743e63fd502237fa693a8f058f
leweili <leweili@163.com>
2024-07-13 15:06:46 +0800

    Update termux-http.sh

--- termux-http.sh
+++ termux-http.sh
@@ -16,6 +16,9 @@ echo '#!/data/data/com.termux/files/usr/bin/sh' > $PREFIX/var/service/http/run
 echo 'exec 2>&1' >> $PREFIX/var/service/http/run
 echo 'cd ~ && python server.py' >> $PREFIX/var/service/http/run
 chmod a+x $PREFIX/var/service/http/run
-sv enable http 
+cd $PREFIX/var/service
+sv-enable http 
 sv up http 
 echo '请重启termux'
+cd ~ 
+pkill termux-boot
